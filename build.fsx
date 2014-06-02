5177435f522a41914b08537f80b96fad491d3361
panesofglass <ryan.riley@panesofglass.org>
2014-06-01 22:11:09 -0500

    Fix NuGet package path.

--- build.fsx
+++ build.fsx
@@ -159,7 +159,7 @@ Target "NuGet" (fun _ ->
             OutputPath = "bin"
             AccessKey = getBuildParamOrDefault "nugetkey" ""
             Publish = hasBuildParam "nugetkey"
-            Dependencies = ["Microsoft.Net.Http", GetPackageVersion "../packages" "Microsoft.Net.Http" |> RequireExactly]
+            Dependencies = ["Microsoft.Net.Http", GetPackageVersion "packages" "Microsoft.Net.Http" |> RequireExactly]
             Files = [ (@"..\bin\Dyfrig.dll", Some "lib/net40", None)
                       (@"..\bin\Dyfrig.xml", Some "lib/net40", None)
                       (@"..\bin\Dyfrig.pdb", Some "lib/net40", None) ] })
