48766cd60bf7e85923f93e06a7e2c6e59251ab9e
kolektiv <andrew@xyncro.com>
2014-08-11 16:17:31 +0100

    OWIN monad, including operators and FSharpx lens based access. breaking changes to helper methods (module rename, etc.)

--- src/Dyfrig/OwinApp.fs
+++ src/Dyfrig/OwinApp.fs
@@ -19,6 +19,8 @@ namespace Dyfrig
 open System
 open System.Collections.Generic
 open System.Threading.Tasks
+open FSharpx
+open FSharpx.Async
 
 /// OWIN environment dictionary
 type OwinEnv = IDictionary<string, obj>
@@ -29,10 +31,135 @@ type OwinApp = OwinEnv -> Async<unit>
 /// OWIN AppFunc signature
 type OwinAppFunc = Func<OwinEnv, Task>
 
+/// OWIN monad implementation
+[<AutoOpen>]
+module Monad =
+
+    /// OWIN monad signature
+    type OwinMonad<'T> = 
+        OwinEnv -> Async<'T * OwinEnv>
+
+    /// OWIN monad builder
+    type OwinMonadBuilder () =
+
+        member x.Return t : OwinMonad<_> = 
+            fun e -> async.Return (t, e)
+
+        member x.ReturnFrom f : OwinMonad<_> = 
+            f
+
+        member x.Zero () : OwinMonad<unit> = 
+            x.Return ()
+
+        member x.Bind (m: OwinMonad<_>, k: _ -> OwinMonad<_>) : OwinMonad<_> =
+            fun e -> m e >>= fun (result, e) -> (k result) e
+
+        member x.Delay (f: unit -> OwinMonad<_>) : OwinMonad<'T> = 
+            x.Bind (x.Return (), f)
+
+        member x.Combine (m1: OwinMonad<_>, m2: OwinMonad<_>) : OwinMonad<_> = 
+            x.Bind (m1, fun () -> m2)
+
+        member x.TryWith (body: OwinMonad<_>, handler: exn -> OwinMonad<_>) : OwinMonad<_> =
+            fun e -> try body e with ex -> handler ex e
+
+        member x.TryFinally (body: OwinMonad<_>, handler) : OwinMonad<_> =
+            fun e -> try body e finally handler ()
+
+        member x.Using (resource: #IDisposable, body: _ -> OwinMonad<_>) : OwinMonad<_> =
+            x.TryFinally (body resource, fun () ->
+                match box resource with
+                | null -> ()
+                | _ -> resource.Dispose ())
+
+        member x.While (guard, body: OwinMonad<_>) : OwinMonad<unit> =
+            match guard () with
+            | true -> x.Bind (body, fun () -> x.While (guard, body))
+            | _ -> x.Return ()
+
+        member x.For (s: seq<_>, body: _ -> OwinMonad<_>) : OwinMonad<unit> =
+            x.Using (s.GetEnumerator (),
+                (fun enum ->
+                    x.While (enum.MoveNext, x.Delay (fun () ->
+                        body enum.Current))))
+
+    /// OWIN monad
+    let owin = OwinMonadBuilder ()
+
+    /// Get a value from the OwinEnv using a lens
+    let get l : OwinMonad<_> = 
+        fun s -> async { return Lens.get s l, s }
+
+    /// Set a value in the OwinEnv using a lens
+    let set l v : OwinMonad<unit> =
+        fun s -> async { return (), Lens.set v s l }
+
+    /// Update a value in the OwinEnv using a lens
+    let update l f : OwinMonad<unit> =
+        fun s -> async { return (), Lens.update f l s }
+
+/// OWIN monad functions
+module Owin =
+
+    let async f : OwinMonad<_> =
+        fun s -> async { return! flip tuple2 s <!> f }
+
+    let compose m f =
+        fun x ->
+            owin {
+                let! v = m x
+                return! f v }
+
+    let composeSeq m f =
+        owin {
+            let! v = m
+            return! f v }
+
+    let map f m =
+        owin {
+            let! v = m
+            return f v }
+
+/// OWIN operators
+module Operators =
+
+    /// Left to right composition
+    let inline (>>=) m f = Owin.composeSeq m f
+
+    /// Right to left composition
+    let inline (=<<) f m = Owin.composeSeq m f
+
+    /// Left to right Kleisli composition
+    let inline (>=>) m f = Owin.compose m f
+
+    /// Right to left Kleisli composition
+    let inline (<=<) f m = Owin.compose m f
+
+    /// Map of f over m
+    let inline (<!>) f m = Owin.map f m
+
+    /// Right to left set value using lens
+    let inline (-->) v l = set l v
+
+    /// Left to right set value using lens
+    let inline (<--) l v = set l v
+
+    /// Right to left update value using lens
+    let inline (-!>) f l = update l f
+
+    /// Left to right update value using lens
+    let inline (<!-) l f = update l f
+
 /// .NET language interop helpers
 [<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
-module OwinApp =
+module OwinAppFunc =
+
     /// Converts a F# Async-based OWIN AppFunc to a standard Func<_,Task> AppFunc.
-    [<CompiledName("ToAppFunc")>]
-    let toAppFunc (app: OwinApp) =
-        OwinAppFunc(fun env -> Async.StartAsTask (app env) :> Task)
+    [<CompiledName("FromOwinApp")>]
+    let fromOwinApp (app: OwinApp) =
+        OwinAppFunc (fun env -> Async.StartAsTask (app env) :> Task)
+
+    /// Converts a F# OwinMonad<_> to a standard Func<_,Task> AppFunc.
+    [<CompiledName("FromOwinMonad")>]
+    let fromOwinMonad (monad: OwinMonad<_>) =
+        fromOwinApp (fun e -> async { do! monad e |> Async.Ignore })
