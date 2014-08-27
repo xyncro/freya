0afaab301c1c6e2be03479cc7c09dc34eed84c5e
Ryan Riley <ryan@tachyus.com>
2014-08-27 10:54:54 -0500

    Re-org and cleanup OwinApp signatures.

--- src/Dyfrig/OwinApp.fs
+++ src/Dyfrig/OwinApp.fs
@@ -1,6 +1,6 @@
 ﻿//----------------------------------------------------------------------------
 //
-// Copyright (c) 2013-2014 Ryan Riley (@panesofglass)
+// Copyright (c) 2013-2014 Ryan Riley (@panesofglass) and Andrew Cherry (@kolektiv)
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -20,7 +20,6 @@ open System
 open System.Collections.Generic
 open System.Threading.Tasks
 open FSharpx
-open FSharpx.Async
 
 /// OWIN environment dictionary
 type OwinEnv = IDictionary<string, obj>
@@ -31,13 +30,28 @@ type OwinApp = OwinEnv -> Async<unit>
 /// OWIN AppFunc signature
 type OwinAppFunc = Func<OwinEnv, Task>
 
-/// OWIN monad implementation
-[<AutoOpen>]
-module Monad =
+/// OWIN monad functions and builder
+[<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
+module OwinMonad =
+    open FSharpx.Async
 
     /// OWIN monad signature
-    type OwinMonad<'T> = 
-        OwinEnv -> Async<'T * OwinEnv>
+    type OwinMonad<'T> = OwinEnv -> Async<'T * OwinEnv>
+
+    let fromAsync f : OwinMonad<_> =
+        fun s -> async { return! flip tuple2 s <!> f }
+
+    let result x : OwinMonad<_> =
+        fun e -> async.Return (x, e)
+
+    let bind (f: 'a -> OwinMonad<'b>) (m: OwinMonad<'a>) : OwinMonad<'b> =
+        fun e -> m e >>= fun (v, e) -> (f v) e
+
+    let compose m f x : OwinMonad<_> =
+        m x |> bind (fun v -> f v)
+
+    let map f m : OwinMonad<_> =
+        m |> bind (fun v -> result (f v))
 
     /// OWIN monad builder
     type OwinMonadBuilder () =
@@ -45,39 +59,39 @@ module Monad =
         member x.Return t : OwinMonad<_> = 
             fun e -> async.Return (t, e)
 
-        member x.ReturnFrom f : OwinMonad<_> = 
+        member x.ReturnFrom f: OwinMonad<_> =
             f
 
-        member x.Zero () : OwinMonad<unit> = 
+        member x.Zero () =
             x.Return ()
 
-        member x.Bind (m: OwinMonad<_>, k: _ -> OwinMonad<_>) : OwinMonad<_> =
-            fun e -> m e >>= fun (result, e) -> (k result) e
+        member x.Bind (m, k) : OwinMonad<_> =
+            bind k m
 
-        member x.Delay (f: unit -> OwinMonad<_>) : OwinMonad<'T> = 
+        member x.Delay f =
             x.Bind (x.Return (), f)
 
-        member x.Combine (m1: OwinMonad<_>, m2: OwinMonad<_>) : OwinMonad<_> = 
+        member x.Combine (m1, m2) =
             x.Bind (m1, fun () -> m2)
 
-        member x.TryWith (body: OwinMonad<_>, handler: exn -> OwinMonad<_>) : OwinMonad<_> =
+        member x.TryWith (body, handler) : OwinMonad<_> =
             fun e -> try body e with ex -> handler ex e
 
-        member x.TryFinally (body: OwinMonad<_>, handler) : OwinMonad<_> =
+        member x.TryFinally (body, handler) : OwinMonad<_> =
             fun e -> try body e finally handler ()
 
-        member x.Using (resource: #IDisposable, body: _ -> OwinMonad<_>) : OwinMonad<_> =
+        member x.Using (resource: #IDisposable, body) : OwinMonad<_> =
             x.TryFinally (body resource, fun () ->
                 match box resource with
                 | null -> ()
                 | _ -> resource.Dispose ())
 
-        member x.While (guard, body: OwinMonad<_>) : OwinMonad<unit> =
+        member x.While (guard, body) : OwinMonad<unit> =
             match guard () with
             | true -> x.Bind (body, fun () -> x.While (guard, body))
             | _ -> x.Return ()
 
-        member x.For (s: seq<_>, body: _ -> OwinMonad<_>) : OwinMonad<unit> =
+        member x.For (s: seq<_>, body) : OwinMonad<unit> =
             x.Using (s.GetEnumerator (),
                 (fun enum ->
                     x.While (enum.MoveNext, x.Delay (fun () ->
@@ -98,45 +112,25 @@ module Monad =
     let update l f : OwinMonad<unit> =
         fun s -> async { return (), Lens.update f l s }
 
-/// OWIN monad functions
-module Owin =
-
-    let async f : OwinMonad<_> =
-        fun s -> async { return! flip tuple2 s <!> f }
-
-    let compose m f =
-        fun x ->
-            owin {
-                let! v = m x
-                return! f v }
-
-    let composeSeq m f =
-        owin {
-            let! v = m
-            return! f v }
-
-    let map f m =
-        owin {
-            let! v = m
-            return f v }
-
 /// OWIN operators
+[<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
 module Operators =
+    open OwinMonad
 
     /// Left to right composition
-    let inline (>>=) m f = Owin.composeSeq m f
+    let inline (>>=) m f = bind f m
 
     /// Right to left composition
-    let inline (=<<) f m = Owin.composeSeq m f
+    let inline (=<<) f m = bind f m
 
     /// Left to right Kleisli composition
-    let inline (>=>) m f = Owin.compose m f
+    let inline (>=>) m f = compose m f
 
     /// Right to left Kleisli composition
-    let inline (<=<) f m = Owin.compose m f
+    let inline (<=<) f m = compose m f
 
     /// Map of f over m
-    let inline (<!>) f m = Owin.map f m
+    let inline (<!>) f m = map f m
 
     /// Right to left set value using lens
     let inline (-->) v l = set l v
@@ -161,5 +155,5 @@ module OwinAppFunc =
 
     /// Converts a F# OwinMonad<_> to a standard Func<_,Task> AppFunc.
     [<CompiledName("FromOwinMonad")>]
-    let fromOwinMonad (monad: OwinMonad<_>) =
+    let fromOwinMonad (monad: OwinMonad.OwinMonad<_>) =
         fromOwinApp (fun e -> async { do! monad e |> Async.Ignore })
