32ac4278788481af32f398c3607020f1a5233a52
kolektiv <andrew@xyncro.com>
2014-10-18 20:13:52 +0100

    tweaks

--- src/Dyfrig.Http/Presets.fs
+++ src/Dyfrig.Http/Presets.fs
@@ -5,6 +5,7 @@ module Dyfrig.Http.Presets
 [<RequireQualifiedAccess>]
 module Charsets =
 
+    /// Convenience definition for "unicode-1-1"
     let Unicode =
         Charset "unicode-1-1"
 
@@ -12,9 +13,11 @@ module Charsets =
 [<RequireQualifiedAccess>]
 module Encodings =
 
+    /// Convenience definition for "deflate"
     let Deflate =
         Encoding "deflate"
 
+    /// Convenience definition for "gzip"
     let GZip =
         Encoding "gzip"
 
@@ -22,10 +25,17 @@ module Encodings =
 [<RequireQualifiedAccess>]
 module MediaTypes =
 
+    /// Convenience definition for "application/json" without extra parameters
     let JSON =
         { MediaType = MediaType (Type "application", SubType "json")
           Parameters = Map.empty }
 
+    /// Convenience definition for "text/plain" without extra parameters
+    let Text =
+        { MediaType = MediaType (Type "text", SubType "plain")
+          Parameters = Map.empty }
+
+    /// Convenience definition for "application/xml" without extra parameters
     let XML =
         { MediaType = MediaType (Type "application", SubType "xml")
           Parameters = Map.empty }
