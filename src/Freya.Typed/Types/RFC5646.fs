f44309d1def8ad3673c03a3743460e0cc223a85d
kolektiv <andrew@xyncro.com>
2014-11-21 11:26:59 +0000

    complete for now implementation of language tags with basic round trip testing for RFC example forms

--- src/Freya.Typed/Types/RFC5646.fs
+++ src/Freya.Typed/Types/RFC5646.fs
@@ -12,6 +12,18 @@ open FParsec
 
    Taken from [http://tools.ietf.org/html/rfc5646] *)
 
+(* Note: The current implementation does not implement either private use
+   tags or grandfathered tags. Contributions are welcome if this is a real
+   world issue for anyone.
+
+   In addition, the main implementation of a language tag does not implement
+   the "extension" property, or the optional "privateuse" property. As the RFC,
+   even in the list of example language tags, never produces an example of either
+   of these in use they have been assumed to be of low importance for now.
+
+   However, if someone does show a valid and even slightly common use case,
+   they will be implemented. *)
+
 (* Helpers *)
 
 let private isAlpha =
@@ -20,16 +32,32 @@ let private isAlpha =
 let private isDigit =
     ((?>) RFC5234.digit)
 
+let private isAlphaNum x =
+    (RFC5234.alpha ?> x || RFC5234.digit ?> x)
+
 let private alphaP min max =
     manyMinMaxSatisfy min max isAlpha .>>? notFollowedBy (skipSatisfy isAlpha)
 
 let private digitP min max =
     manyMinMaxSatisfy min max isDigit .>>? notFollowedBy (skipSatisfy isDigit)
 
+let private alphaNumP min max =
+    manyMinMaxSatisfy min max isAlphaNum .>>? notFollowedBy (skipSatisfy isAlphaNum)
+
 (* Language *)
 
 type Language =
     | Language of string * string list option
+
+let private extF =
+    function | x -> append "-" >> append x
+
+let private extLangF =
+    function | xs -> join id extF xs
+
+let private languageF =
+    function | Language (x, Some e) -> append x >> extLangF e
+             | Language (x, _) -> append x 
 
 let private extP =
     skipChar '-' >>. alphaP 3 3
@@ -48,16 +76,25 @@ let private languageP =
 
 type Language with
 
+    static member Format =
+        format languageF
+
     static member Parse =
         parseExact languageP
 
     static member TryParse =
         parseOption languageP
 
+    override x.ToString () =
+        Language.Format x
+
 (* Script *)
 
 type Script =
     | Script of string
+
+let private scriptF =
+    function | Script x -> append "-" >> append x 
 
 let private scriptP =
     skipChar '-' >>. alphaP 4 4 |>> Script
@@ -67,27 +104,74 @@ let private scriptP =
 type Region =
     | Region of string
 
+let private regionF =
+    function | Region x -> append "-" >> append x
+
 let private regionP =
     skipChar '-' >>. (alphaP 2 2 <|> digitP 3 3) |>> Region
+
+(* Variant *)
+
+type Variant =
+    | Variant of string list
+
+let private varF =
+    function | x -> append "-" >> append x
+
+let private variantF =
+    function | Variant xs -> join id varF xs
+
+let private alphaPrefixVariantP =
+    alphaNumP 5 8
+
+let private digitPrefixVariantP =
+    satisfy isDigit .>>. alphaNumP 3 3 |>> fun (c, s) -> sprintf "%c%s" c s
+
+let private varP =
+    skipChar '-' >>. (alphaPrefixVariantP <|> digitPrefixVariantP)
+
+let private variantP =
+    many varP |>> Variant
 
 (* Language Tag *)
 
 type LanguageTag =
     { Language: Language
       Script: Script option
-      Region: Region option }
+      Region: Region option
+      Variant: Variant }
+
+let private languageTagF =
+    function | { Language = language
+                 Script = script
+                 Region = region
+                 Variant = variant } ->
+                 let formatters =
+                    [ languageF language
+                      (function | Some x -> scriptF x | _ -> id) script
+                      (function | Some x -> regionF x | _ -> id) region
+                      variantF variant ]
+
+                 fun b -> List.fold (fun b f -> f b) b formatters
 
 let private languageTagP =
-    tuple3 languageP (opt scriptP) (opt regionP)
-    |>> fun (language, script, region) ->
+    tuple4 languageP (opt (attempt scriptP)) (opt (attempt regionP)) (variantP)
+    |>> fun (language, script, region, variant) ->
         { Language = language
           Script = script
-          Region = region }
+          Region = region
+          Variant = variant }
 
 type LanguageTag with
+
+    static member Format =
+        format languageTagF
 
     static member Parse =
         parseExact languageTagP
 
     static member TryParse =
         parseOption languageTagP
+
+    override x.ToString () =
+        LanguageTag.Format x
--- tests/Freya.Typed.Tests/RFC5646.Tests.fs
+++ tests/Freya.Typed.Tests/RFC5646.Tests.fs
@@ -5,23 +5,92 @@ open Swensen.Unquote
 open Freya.Typed
 
 [<Test>]
-let ``LanguageTag Parsing`` () =
+let ``LanguageTag Formatting/Parsing`` () =
 
-    let definitions =
-        [ "de", 
-          { Language = Language ("de", None)
-            Script = None
-            Region = None }
-                  
-          "zh-Hant", 
-          { Language = Language ("zh", None)
-            Script = Some (Script "Hant")
-            Region = None }
-                  
-          "zh-cmn-Hans-CN",
-          { Language = Language ("zh", Some [ "cmn" ])
-            Script = Some (Script "Hans")
-            Region = Some (Region "CN") } ]
+    (* Language *)
 
-    definitions
-    |> List.iter (fun (s, p) -> LanguageTag.TryParse s =? Some p)
+    let langTyped =
+        { Language = Language ("de", None)
+          Script = None
+          Region = None
+          Variant = Variant [] }
+
+    let langString =
+        "de"
+
+    (* Language + Script *)
+
+    let langScriptTyped =
+        { Language = Language ("zh", None)
+          Script = Some (Script "Hant")
+          Region = None
+          Variant = Variant [] }
+
+    let langScriptString =
+        "zh-Hant"
+
+    (* Extended Language + Script + Region *)
+
+    let langScriptRegionTyped =
+        { Language = Language ("zh", Some [ "cmn" ])
+          Script = Some (Script "Hans")
+          Region = Some (Region "CN")
+          Variant = Variant [] }
+
+    let langScriptRegionString =
+        "zh-cmn-Hans-CN"
+
+    (* Language + Variant *)
+
+    let langVariantTyped =
+        { Language = Language ("sl", None)
+          Script = None
+          Region = None
+          Variant = Variant [ "rozaj" ] }
+
+    let langVariantString =
+        "sl-rozaj"
+
+    (* Language + Region + Variant *)
+
+    let langRegionVariantTyped =
+        { Language = Language ("de", None)
+          Script = None
+          Region = Some (Region "CH")
+          Variant = Variant [ "1901" ] }
+
+    let langRegionVariantString =
+        "de-CH-1901"
+
+    (* Language + Script + Region + Variant *)
+
+    let langScriptRegionVariantTyped =
+        { Language = Language ("hy", None)
+          Script = Some (Script "Latn")
+          Region = Some (Region "IT")
+          Variant = Variant [ "arevela" ] }
+
+    let langScriptRegionVariantString =
+        "hy-Latn-IT-arevela"
+
+    (* Language + Region *)
+
+    let langRegionTyped =
+        { Language = Language ("de", None)
+          Script = None
+          Region = Some (Region "DE")
+          Variant = Variant [] }
+
+    let langRegionString =
+        "de-DE"
+
+    (* Round trip *)
+
+    roundTrip (LanguageTag.Format, LanguageTag.Parse) [
+        langTyped,                    langString
+        langScriptTyped,              langScriptString
+        langScriptRegionTyped,        langScriptRegionString
+        langVariantTyped,             langVariantString
+        langRegionVariantTyped,       langRegionVariantString
+        langScriptRegionVariantTyped, langScriptRegionVariantString
+        langRegionTyped,              langRegionString ]
