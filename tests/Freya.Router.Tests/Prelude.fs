525dfa5aeebdd8362ca076edda4b3d8615b3c355
kolektiv <andrew@xyncro.com>
2014-11-25 17:22:10 +0000

    start of router tests, may fix some of travis build...

---
+++ tests/Freya.Router.Tests/Prelude.fs
@@ -0,0 +1,39 @@
+﻿[<AutoOpen>]
+module internal Freya.Router.Tests.Prelude
+
+open System.Collections.Generic
+open Aether.Operators
+open Freya.Core
+open Freya.Core.Operators
+open Freya.Router
+open Freya.Typed
+
+(* Keys *)
+
+let [<Literal>] private testKey =
+    "freya.Test"
+
+(* Lenses *)
+
+let private testLens =
+    dictLens testKey <--> (unbox<int>, box)
+
+(* Functions *)
+
+let get : Freya<int> =
+    getLM testLens
+
+let set i =
+    setLM testLens i
+
+let private run path m =
+    let state = Dictionary<string, obj> ()
+    let router = compileFreyaRouter m
+
+    Async.RunSynchronously ((setLM Request.path path *> router) state)
+
+let result path m =
+    fst (run path m)
+
+let state path m =
+    snd (run path m)
---
+++ tests/Freya.Router.Tests/Router.Tests.fs
@@ -0,0 +1,11 @@
+﻿module Freya.Router.Tests.Router
+
+open NUnit.Framework
+open Swensen.Unquote
+open Freya.Pipeline
+open Freya.Router
+
+[<Test>]
+let ``Router With No Routes Returns Next`` () =
+    result "/" (freyaRouter { return () }) =? Next
+
