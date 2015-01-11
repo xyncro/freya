2571e35ef4b2e52e0d6cffe55542e175e6c4409d
panesofglass <ryan.riley@panesofglass.org>
2015-01-11 15:43:41 -0600

    Split Operators.fs into two files and add some operators tests.

--- src/Freya.Core/Operators.fs
+++ src/Freya.Core/Operators.fs
@@ -17,79 +17,32 @@
 // limitations under the License.
 //----------------------------------------------------------------------------
 
-namespace Freya.Core
-
-/// Core combinator definitions for <see cref="Freya{T}" /> computations.
-[<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
-module Freya =
-
-    /// Wraps a value x in a <see cref="Freya{T}" /> computation.
-    let inline returnM x : Freya<'T> = 
-        fun env -> 
-            async.Return (x, env)
-
-    /// Applies a function of a value to an <see cref="Async{T}" /> result
-    /// into a <see cref="Freya{T}" /> computation.
-    let inline asyncM f =
-        (fun f -> 
-            fun env -> 
-                async { 
-                    let! v = f
-                    return v, env }) << f
-
-    /// Binds a <see cref="Freya{T}" /> computation with a function that
-    /// takes the value from the <see cref="Freya{T}" /> computation and
-    /// computes a new <see cref="Freya{T}" /> computation of a possibly
-    /// different type.
-    let inline bind m f : Freya<'T> =
-        fun s -> 
-            async { 
-                let! r, s = m s
-                return! (f r) s }
-
-    /// Applies a function wrapped in a <see cref="Freya{T}" /> computation
-    /// onto a <see cref="Freya{T}" /> computation value.
-    let inline apply f m : Freya<'T> =
-        bind f (fun f' ->
-        bind m (fun m' ->
-        returnM (f' m')))
-
-    /// Applies a function taking one arguments to one <see cref="Freya{T}" /> computations.
-    let inline map f m : Freya<'T> =
-        bind m (fun m' ->
-        returnM (f m'))
-
-    /// Applies a function taking two arguments to two <see cref="Freya{T}" /> computations.
-    let inline map2 f m1 m2 =
-        apply (apply (returnM f) m1) m2
-
-
 /// Custom operators for composing <see cref="Freya{T}" /> computations.
-module Operators =
+module Freya.Core.Operators
 
-    let inline (>>=) m f =
-        Freya.bind m f
+let inline (>>=) m f =
+    Freya.bind m f
 
-    let inline (=<<) f m =
-        Freya.bind m f
+let inline (=<<) f m =
+    Freya.bind m f
 
-    let inline (<*>) f m =
-        Freya.apply f m
+let inline (<*>) f m =
+    Freya.apply f m
 
-    let inline (<!>) f m =
-        Freya.map f m
+let inline (<!>) f m =
+    Freya.map f m
 
-    let inline ( *>) m1 m2 =
-        Freya.map2 (fun _ x -> x) m1 m2
+let inline ( *>) m1 m2 =
+    Freya.map2 (fun _ x -> x) m1 m2
 
-    let inline ( <*) m1 m2 =
-        Freya.map2 (fun x _ -> x) m1 m2
+let inline ( <*) m1 m2 =
+    Freya.map2 (fun x _ -> x) m1 m2
 
-    let inline (>>.) m f =
-        Freya.bind m (fun _ -> f)
+let inline (>>.) m f =
+    Freya.bind m (fun _ -> f)
 
-    let inline (>=>) m1 m2 =
-        fun x -> m1 x >>= m2
+let inline (>=>) m1 m2 =
+    fun x -> m1 x >>= m2
 
-    let inline (<=<) m1 m2 =
-        fun x -> m2 x >>= m1
+let inline (<=<) m1 m2 =
+    fun x -> m2 x >>= m1
--- tests/Freya.Core.Tests/Core.Tests.fs
+++ tests/Freya.Core.Tests/Core.Tests.fs
@@ -23,6 +23,8 @@ let ``getLM, setLM, modLM behave correctly`` () =
 
     fst result =? (42, 84)
 
+(* Integration *)
+
 open Freya.Core.Integration
 open System.Threading.Tasks
 
@@ -46,9 +48,7 @@ let ``freya computation can compose with an OwinAppFunc`` () =
 
 [<Test>]
 let ``freya computation can roundtrip to and from OwinAppFunc`` () =
-    let app =
-        freya {
-            do! setLM answerLens 42 }
+    let app = setLM answerLens 42
 
     let converted =
         app
@@ -62,4 +62,40 @@ let ``freya computation can roundtrip to and from OwinAppFunc`` () =
             return v1 }
     
     let result = run m
+    fst result =? 42
+
+(* Operators *)
+
+open Freya.Core.Operators
+
+[<Test>]
+let ``apply correctly unwraps and applies Freya computations`` () =
+    let comp = Freya.returnM 2
+    let f = Freya.returnM ((+) 40)
+    let test = Freya.apply f comp
+    let result = run test
+    fst result =? 42
+
+[<Test>]
+let ``map correctly applies a function to a Freya computation`` () =
+    let test =
+        Freya.returnM 40
+        |> Freya.map ((+) 2)
+    let result = run test
+    fst result =? 42
+
+[<Test>]
+let ``map2 correctly applies a function to Freya computations`` () =
+    let comp1 = Freya.returnM 2
+    let comp2 = Freya.returnM 40
+    let test = Freya.map2 (+) comp1 comp2
+    let result = run test
+    fst result =? 42
+
+[<Test>]
+let ``map + apply correctly chain across applicative computations`` () =
+    let comp1 = Freya.returnM 2
+    let comp2 = Freya.returnM 40
+    let test = (+) <!> comp1 <*> comp2
+    let result = run test
     fst result =? 42
