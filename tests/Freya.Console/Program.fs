f91fe480835342c5bd35a8c42905746ab9781e76
Ryan Riley <ryanriley@live.com>
2015-12-04 11:39:00 -0800

    Add additional tests to verify headers and body writing

--- tests/Freya.Console/Program.fs
+++ tests/Freya.Console/Program.fs
@@ -4,33 +4,46 @@ open Arachne.Language
 open Arachne.Http
 open Freya.Core
 open Freya.Core.Operators
+open Freya.Inspector
 open Freya.Lenses.Http
 open Freya.Machine
 open Freya.Machine.Extensions.Http
+open Freya.Machine.Inspector
 open Freya.Machine.Router
 open Freya.Router
+open Freya.Router.Inspector
 open Suave.Logging
 open Suave.Owin
 open Suave.Types
 open Suave.Web
 
-// Resources
+(* Resources *)
+
+let en = LanguageTag.Parse "en"
 
 let inline represent (x : string) =
     { Description =
         { Charset = Some Charset.Utf8
           Encodings = None
           MediaType = Some MediaType.Text
-          Languages = Some [ LanguageTag.Parse "en" ] }
+          Languages = Some [ en ] }
       Data = Encoding.UTF8.GetBytes x }
 
 let ok =
         Freya.Optic.set Response.reasonPhrase_ (Some "Hey Folks!")
      *> Freya.init (represent "Hey, folks!")
 
+let common =
+    freyaMachine {
+        using http
+        charsetsSupported Charset.Utf8
+        languagesSupported en
+        mediaTypesSupported MediaType.Text }
+
 let home =
     freyaMachine {
         using http
+        //including common
         methodsSupported GET
         handleOk ok }
 
@@ -38,12 +51,29 @@ let routes =
     freyaRouter {
         resource "/" home }
 
+(* Inspectors *)
+
+let inspectorConfig =
+    { Inspectors =
+        [ freyaMachineInspector
+          freyaRouterInspector ] }
+
+let inspect =
+    freyaInspector inspectorConfig
+
+(* API *)
+
+let api =
+    inspect >?= routes
+
+(* Suave *)
+
 let config =
     { defaultConfig with
         bindings = [ HttpBinding.mk' HTTP "0.0.0.0" 7000 ]
         logger = Loggers.saneDefaultsFor LogLevel.Verbose }
 
-let owin = OwinApp.ofAppFunc "/" (OwinAppFunc.ofFreya routes)
+let owin = OwinApp.ofAppFunc "/" (OwinAppFunc.ofFreya api)
 
 // Main
 
